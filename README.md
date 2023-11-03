d05c6c422c442d24c77d4a77cc8e9c411846fefe
Tomin Roman <tominroman128@gmail.com>
2023-11-03 23:07:10 +0300

    создал карточки и могу их удалить

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/RomTomGreat/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
-
+const card = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const cardPlaceList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+function createCards(item) {
+  const cardList = card.querySelector('.card').cloneNode(true);
 
+  const cardImage = cardList.querySelector('.card__image');
+  cardImage.src = item.link;
+
+  const cardDelete = cardList.querySelector('.card__delete-button');
 // @todo: Функция удаления карточки
+  cardDelete.addEventListener('click', function() {
+    const trash = cardDelete.closest('.card');
+    trash.remove();
+  });
 
+  const cardFooterContainer = cardList.querySelector('.card__description');
+
+  const cardTitle = cardFooterContainer.querySelector('.card__title');
+  cardTitle.textContent = item.name;
+
+  const cardLike = cardFooterContainer.querySelector('.card__like-button');
+
+  return cardList;
+}
 // @todo: Вывести карточки на страницу
+function showCards() {
+  initialCards.forEach((item) => {
+
+    const createCardsShow = createCards(item);
+    cardPlaceList.append(createCardsShow);
+
+  });
+}
+
+showCards();
